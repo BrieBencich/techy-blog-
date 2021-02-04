379e68af879c2b3b500f3ae14cd36f37a85d2be2
Brie Bencich <bbencich@hotmail.com>
2021-02-04 19:22:04 +0200

    adding in Post model

---
+++ models/index.js
@@ -0,0 +1,72 @@
+const Comment = require('./Comment'); 
+const User = require('./User'); 
+const Vote = require('./Vote'); 
+const Post = require('./Post'); 
+
+User.hasMany(Post, { 
+    foreignKey: 'user_id'
+}); 
+
+Post.belongsToMany(Post, { 
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+}); 
+
+User.belongsToMany(Post, { 
+    through: Vote, 
+    as: 'voted_posts',
+    foreignKey: 'user_id', 
+    onDelete: 'SET NULL'
+}); 
+
+Post.belongsToMany(User, { 
+    through: Vote, 
+    as: 'voted_posts', 
+    foreignKey: 'post_id', 
+    onDelete: 'SET NULL'
+}); 
+
+Vote.belongsToMany(User, { 
+    foreignKey: 'user_id', 
+    onDelete: 'SET NULL'
+}); 
+
+Vote.belongsToMany(Post, { 
+    foreignKey: 'post_id', 
+    onDelete: 'SET NULL'
+}); 
+
+User.hasMany(Vote, { 
+    foreignKey: 'user_id'
+}); 
+
+Post.hasMany(Vote, { 
+    foreignKey: 'post_id'
+}); 
+
+Comment.belongsTo(User, { 
+    foreignKey: 'user_id', 
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, { 
+    foreignKey: 'post_id', 
+    onDelete: 'SET NULL'
+}); 
+
+User.hasMany(Comment, { 
+    foreignKey: 'user_id', 
+    onDelete: 'SET NULL'
+}); 
+
+Post.hasMany(Comment, { 
+    foreignKey: 'post_id'
+}); 
+
+module.exports = { User, Post, Vote, Comment }; 
+
+
+
+
+
+
